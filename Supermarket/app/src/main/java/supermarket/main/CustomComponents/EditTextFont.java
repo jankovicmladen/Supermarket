a3fcb4a9bea5d93663f9eedc35409a9d0bf91acb
Mladen <mladen.ns@gmail.com>
2016-09-07 18:53:27 +0200

    fags

---
+++ Supermarket/app/src/main/java/supermarket/main/CustomComponents/EditTextFont.java
@@ -0,0 +1,23 @@
+package supermarket.main.CustomComponents;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ * Created by Mladen on 9/7/2016.
+ */
+public class EditTextFont extends EditText {
+    public EditTextFont(Context context) {
+        super(context);
+    }
+
+    public EditTextFont(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EditTextFont(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
