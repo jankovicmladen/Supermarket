60689c0c255ab086ba507e42489e4f618a54168d
Mladen <mladen.ns@gmail.com>
2016-09-09 10:54:54 +0200

    10.09.

--- Supermarket/app/src/main/java/supermarket/main/LoginActivity.java
+++ Supermarket/app/src/main/java/supermarket/main/LoginActivity.java
@@ -1,5 +1,15 @@
 package supermarket.main;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -10,13 +20,21 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import jp.wasabeef.glide.transformations.CropCircleTransformation;
+import jp.wasabeef.glide.transformations.internal.Utils;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int YOUR_SELECT_PICTURE_REQUEST_CODE = 111;
 
     private ViewPager mViewPager;
     private TabLayout mTabLayout;
     private ImageView mImageView, mLogoImage, mAddImage;
+    private Uri outputFileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +42,36 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         iniComponents();
+
+        addListeners();
+
+    }
+
+    private void iniComponents() {
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new LoginPagerAdapter(getSupportFragmentManager()));
+
+        mTabLayout = (TabLayout) findViewById(R.id.tab);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        mImageView = (ImageView) findViewById(R.id.image);
+
+        Glide.with(this).load(R.drawable.username)
+                .bitmapTransform(new CropCircleTransformation(getApplicationContext()))
+                .into(mImageView);
+
+        mLogoImage = (ImageView) findViewById(R.id.img_logo);
+        mAddImage = (ImageView) findViewById(R.id.add_image);
+    }
+
+    private void addListeners(){
+
+        mAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImageIntent();
+            }
+        });
 
         mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -49,23 +97,73 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-
     }
 
-    private void iniComponents() {
-        mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        mViewPager.setAdapter(new LoginPagerAdapter(getSupportFragmentManager()));
+    private void openImageIntent() {
 
-        mTabLayout = (TabLayout) findViewById(R.id.tab);
-        mTabLayout.setupWithViewPager(mViewPager);
+// Determine Uri of camera image to save.
+        final File root = new File(Environment.getExternalStorageDirectory() + File.separator + "MyDir" + File.separator);
+        root.mkdirs();
+        final String fname = "img_"+ System.currentTimeMillis() + ".jpg";;
+        final File sdImageMainDirectory = new File(root, fname);
+        outputFileUri = Uri.fromFile(sdImageMainDirectory);
 
-        mImageView = (ImageView) findViewById(R.id.image);
+        // Camera.
+        final List<Intent> cameraIntents = new ArrayList<>();
+        final Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        final PackageManager packageManager = getPackageManager();
+        final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
+        for(ResolveInfo res : listCam) {
+            final String packageName = res.activityInfo.packageName;
+            final Intent intent = new Intent(captureIntent);
+            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            intent.setPackage(packageName);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            cameraIntents.add(intent);
+        }
 
-        Glide.with(this).load(R.drawable.chrysanthemum)
-                .bitmapTransform(new CropCircleTransformation(getApplicationContext()))
-                .into(mImageView);
+        // Filesystem.
+        final Intent galleryIntent = new Intent();
+        galleryIntent.setType("image/*");
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
 
-        mLogoImage = (ImageView) findViewById(R.id.img_logo);
-        mAddImage = (ImageView) findViewById(R.id.add_image);
+        // Chooser of filesystem options.
+        final Intent chooserIntent = Intent.createChooser(galleryIntent, "Select Source");
+
+        // Add the camera options.
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new Parcelable[cameraIntents.size()]));
+
+        startActivityForResult(chooserIntent, YOUR_SELECT_PICTURE_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == YOUR_SELECT_PICTURE_REQUEST_CODE) {
+                final boolean isCamera;
+                if (data == null) {
+                    isCamera = true;
+                } else {
+                    final String action = data.getAction();
+                    if (action == null) {
+                        isCamera = false;
+                    } else {
+                        isCamera = action.equals(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    }
+                }
+
+                Uri selectedImageUri;
+                if (isCamera) {
+                    selectedImageUri = outputFileUri;
+                } else {
+                    selectedImageUri = data == null ? null : data.getData();
+
+                }
+
+                Glide.with(this).load(selectedImageUri)
+                        .bitmapTransform(new CropCircleTransformation(getApplicationContext()))
+                        .into(mImageView);
+            }
+        }
     }
 }
--- Supermarket/app/src/main/java/supermarket/main/SigninFragment.java
+++ Supermarket/app/src/main/java/supermarket/main/SigninFragment.java
@@ -4,17 +4,25 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
 
 
 public class SigninFragment extends Fragment {
 
+    SwitchCompat mSwIsCompany;
+    View mView;
+    RelativeLayout mPIB, mCompanyName;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
     }
 
@@ -22,7 +30,13 @@ public class SigninFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signin, container, false);
+        mView = inflater.inflate(R.layout.fragment_signin, container, false);
+
+        iniComponents();
+
+        addListeners();
+
+        return mView;
     }
 
 
@@ -51,5 +65,28 @@ public class SigninFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private void iniComponents(){
+        mSwIsCompany = (SwitchCompat) mView.findViewById(R.id.isCompany);
+
+        mPIB = (RelativeLayout) mView.findViewById(R.id.pib);
+        mCompanyName = (RelativeLayout) mView.findViewById(R.id.company_name);
+
+    }
+
+    private void addListeners(){
+        mSwIsCompany.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    mCompanyName.setVisibility(View.VISIBLE);
+                    mPIB.setVisibility(View.VISIBLE);
+                }else{
+                    mCompanyName.setVisibility(View.GONE);
+                    mPIB.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
