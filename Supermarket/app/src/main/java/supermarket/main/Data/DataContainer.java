ecdb232b9e564f0556a042ebf239ce426df4d6a3
Mladen <mladen.ns@gmail.com>
2016-09-09 11:50:03 +0200

    10.09.

---
+++ Supermarket/app/src/main/java/supermarket/main/Data/DataContainer.java
@@ -0,0 +1,9 @@
+package supermarket.main.Data;
+
+/**
+ * Created by cubesschool2 on 9/9/16.
+ */
+public class DataContainer {
+    public static String TOKEN;
+
+}
---
+++ Supermarket/app/src/main/java/supermarket/main/constant/Constant.java
@@ -0,0 +1,20 @@
+package supermarket.main.constant;
+
+/**
+ * Created by cubesschool2 on 9/9/16.
+ */
+public class Constant {
+
+    private static String BASE_URL = "http://shop.cubes.rs/";
+
+    public static String GRT_TOKEN_URL = BASE_URL + "phone-home-gettoken";
+    public static String LOGIN_URL = BASE_URL + "phone-user";
+    public static String SIGNUP_URL = BASE_URL + "phone-signup";
+    public static String FORGOT_PASSWORD_URL = BASE_URL + "phone-user-forgotpassword";
+    public static String CITY_URL = BASE_URL + "phone-helper-places";
+    public static String RESERVATION_URL = BASE_URL + "phone-helper-reservation";
+    public static String CATEGORY_URL = BASE_URL + "phone-categories";
+
+    //POSTMAN za proveru post metoda
+
+}
